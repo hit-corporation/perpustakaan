01fe6a4e2f8433e5687ea9ec4819243b24380425
fauziahmad37 <fauzi.enginer@gmail.com>
2023-02-15 11:56:28 +0700

    penyesuaian UI modal update data publisher pada UI publisher index

--- application/views/publisher/index.php
+++ application/views/publisher/index.php
@@ -9,14 +9,13 @@
 
 		<script src="<?=base_url('assets/vendor/jquery/jquery.min.js')?>"></script>
 
-
 		<!-- Begin Page Content -->
 		<div class="container-fluid">
 
 			<!-- Page Heading -->
 			<div class="d-sm-flex align-items-center justify-content-between mb-4">
 				<h1 class="h3 mb-0 text-gray-800">Penerbit</h1>
-				<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+				<a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Data</a>
 			</div>
 
 			<!-- Content Row -->
@@ -34,9 +33,7 @@
 									<tr>
 										<th>Username</th>
 										<th>Full Name</th>
-										<th>Email</th>
-										<th>Role</th>
-										<th>Status</th>
+										<th>Tanggal Dibuat</th>
 										<th>Action</th>
 									</tr>
 								</thead>
@@ -45,13 +42,11 @@
 									<?php $i=1; foreach ($publishers as $key => $value) { ?>
 
 									<tr>
-										<td><?=$value['user_name']?></td>
-										<td><?=$value['full_name']?></td>
-										<td><?=$value['email']?></td>
-										<td><?=$value['role_id']?></td>
-										<td><?=$value['status']?></td>
+										<td><?=$value['publisher_name']?></td>
+										<td><?=$value['address']?></td>
+										<td><?=$value['created_at']?></td>
 										<td>
-											<button type="button" href="<?=base_url('user/edit/'.$value['id'])?>" class="btn btn-primary btn-circle btn-sm" data-toggle="modal" data-target="#editModal<?=$i?>">
+											<button type="button" name="editBtn" id="editBtn" href="<?=base_url('user/edit/'.$value['id'])?>" class="btn btn-primary btn-circle btn-sm" data-toggle="modal" data-target="#editModal<?=$i?>">
 												<i class="fas fa-edit"></i>
 											</button>
 
@@ -70,48 +65,13 @@
 															<div class="form-group">
 																<label for="userName">User Name</label>
 																<input type="hidden" id="id" name="id" value="<?=$value['id']?>">
-																<input type="text" class="form-control" id="userName" name="userName" value="<?=$value['user_name']?>" placeholder="Enter Username">
+																<input type="text" class="form-control" id="userName" name="userName" value="<?=$value['publisher_name']?>" placeholder="Enter Username">
 															</div>
 															<div class="form-group">
 																<label for="fullName">Full Name</label>
-																<input type="text" class="form-control" id="fullName" name="fullName" value="<?=$value['full_name']?>" placeholder="Enter Full Name">
+																<input type="text" class="form-control" id="fullName" name="fullName" value="<?=$value['address']?>" placeholder="Enter Full Name">
 															</div>
-															<div class="form-group">
-																<label for="email">Email</label>
-																<input type="email" class="form-control" id="email" name="email" value="<?=$value['email']?>" placeholder="Enter Your Email">
-															</div>
-															<div class="form-group">
-																<label for="role">Role</label>
-																<select class="form-control" name="role" id="role">
-																	<option value="1" <?=($value['role_id'] == 1) ? 'selected' : '' ?>>Admin</option>
-																	<option value="2" <?=($value['role_id'] == 2) ? 'selected' : '' ?>>User</option>
-																</select>
-															</div>
-															<div class="form-group">
-																<label for="status">Status</label>
-																<select class="form-control" name="status" id="status">
-																	<option value="active" <?=($value['status'] == 'active') ? 'selected' : '' ?>>Active</option>
-																	<option value="inactive" <?=($value['status'] == 'inactive') ? 'selected' : '' ?>>Inactive</option>
-																</select>
-															</div>
-															<div class="form-group">
-																<label for="password">Password</label>
-																<input type="password" class="form-control" id="password<?=$i?>" name="password" value="<?=$value['user_pass']?>" placeholder="Password" readonly>
-															</div>
-															<div class="form-check">
-																<input type="checkbox" class="form-check-input" id="changePassword<?=$i?>" name="changePassword">
-																<label class="form-check-label" for="changePassword">Change Password</label>
-																<script>
-																	$('#changePassword<?=$i?>').click(function() {
-																		if ($(this).is(':checked')) {
-																			$('#password<?=$i?>').removeAttr('readonly');
-																		} else {
-																			$('#password<?=$i?>').attr('readonly', 'readonly');
-																		}
-																	});
-																</script>
-															</div>
-															<!-- <button type="submit" class="btn btn-primary" name="submit">Submit</button> -->
+															
 														
 													</div>
 													<div class="modal-footer">
@@ -163,6 +123,6 @@
 		<!-- /.container-fluid -->
 
 	</div>
-<!-- </div> -->
+
 
 <?php $this->stop() ?>
