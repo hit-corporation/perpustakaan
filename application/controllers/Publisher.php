f64d3534e65cba32dfe6ff6ed05338fe8e31112f
fauziahmad37 <fauzi.enginer@gmail.com>
2023-02-15 13:45:47 +0700

    create proses simpan, delete dan update pada controller publisher

--- application/controllers/Publisher.php
+++ application/controllers/Publisher.php
@@ -8,6 +8,48 @@ class Publisher extends MY_Controller{
 	}
 
 	public function index(){
+		$post = $this->input->post();
+
+		if (isset($post['save'])) {
+			$data = [
+				'publisher_name' => $post['publisher_name'],
+				'address' => $post['address'],
+				'created_at' => date('Y-m-d H:i:s'),
+			];
+			$res = $this->Publisher_model->add($data);
+			if ($res) {
+				$this->session->set_flashdata('success', 'Data berhasil ditambahkan');
+			} else {
+				$this->session->set_flashdata('error', 'Data gagal ditambahkan');
+			}
+		}
+
+		if (isset($post['update'])){
+			$data = [
+				'publisher_name' => $post['publisher_name'],
+				'address' => $post['address'],
+				'updated_at' => date('Y-m-d H:i:s'),
+			];
+			$res = $this->Publisher_model->update($post['id'], $data);
+			if ($res) {
+				$this->session->set_flashdata('success', 'Data berhasil diubah');
+			} else {
+				$this->session->set_flashdata('error', 'Data gagal diubah');
+			}
+		}
+
+		if (isset($post['delete'])){
+			$data = [
+				'deleted_at' => date('Y-m-d H:i:s'),
+			];
+			$res = $this->Publisher_model->update($post['id'], $data);
+			if ($res) {
+				$this->session->set_flashdata('success', 'Data berhasil dihapus');
+			} else {
+				$this->session->set_flashdata('error', 'Data gagal dihapus');
+			}
+		}
+
 		$data['title'] = 'Publisher';
 		$data['publishers'] = $this->Publisher_model->getAll();
 
