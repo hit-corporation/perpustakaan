56ba333c9c4e1d86d52e4668e69e130f56e66f07
fauziahmad37 <fauzi.enginer@gmail.com>
2023-02-15 16:28:44 +0700

    change proccess save and update data user pada controller User

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -16,25 +16,51 @@ class User extends CI_Controller {
 	public function index(){
 		$post = $this->input->post();
 
-		if(isset($post['submit'])){
+		if(isset($post['save'])){
 			$data = [
-				'user_name' => $post['userName'],
-				'full_name' => $post['fullName'],
+				'user_name' => $post['user_name'],
+				'full_name' => $post['full_name'],
 				'email' => $post['email'],
-				'user_pass' => (isset($post['changePassword'])) ? password_hash($post['password'], PASSWORD_DEFAULT) : $post['password'],
-				'role_id' => $post['role'],
+				'user_pass' => $post['password'],
+				'role_id' => $post['user_role'],
 				'status' => $post['status']
 			];
-			$this->User_model->update($post['id'], $data);
-			redirect(base_url('user'));
-			
+			$res = $this->User_model->insert($data);
+			if($res){
+				$this->session->set_flashdata('success', 'User added successfully');
+			}else{
+				$this->session->set_flashdata('error', 'Something went wrong');
+			}
+		}
+
+		if(isset($post['update'])){
+			$data = [
+				'user_name' => $post['user_name'],
+				'full_name' => $post['full_name'],
+				'email' => $post['email'],
+				'user_pass' => isset($post['changePassword']) ? password_hash($post['password'], PASSWORD_DEFAULT) : $post['password'], // if changePassword is set, then change password, else keep the old password (password_hash() is used to encrypt the password
+				'role_id' => $post['user_role'],
+				'status' => $post['status']
+			];
+			$res = $this->User_model->update($post['id'], $data);
+			if($res){
+				$this->session->set_flashdata('success', 'User updated successfully');
+			}else{
+				$this->session->set_flashdata('error', 'Something went wrong');
+			}
+		}
+
+		if(isset($post['delete'])){
+			$res = $this->User_model->delete($post['id']);
+			if($res){
+				$this->session->set_flashdata('success', 'User deleted successfully');
+			}else{
+				$this->session->set_flashdata('error', 'Something went wrong');
+			}
 		}
 
 		$data['users'] = $this->User_model->get_all_users();
-
-		// $this->load->view('header');
-		// $this->load->view('user/index', $data);
-		// $this->load->view('footer');
+		$data['user_role'] = $this->User_model->get_user_role();
 
 		echo $this->template->render('index', $data);
 	}
