5052a02b9b7056389d4b0d53c09f0c7681790055
fauziahmad37 <fauzi.enginer@gmail.com>
2023-02-17 14:12:38 +0700

    fixing Controller Publisher

--- application/controllers/Publisher.php
+++ application/controllers/Publisher.php
@@ -5,6 +5,12 @@ class Publisher extends MY_Controller{
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('publisher_model');
+		$this->load->library('form_validation');
+	}
+
+	public function index(): void
+	{
+		echo $this->template->render('index');
 	}
 
     /**
@@ -31,59 +37,104 @@ class Publisher extends MY_Controller{
         $dataTable = [
             'draw'            => $this->input->get('draw') ?? NULL,
             'data'            => $this->publisher_model->get_all($filter, $limit, $offset),
-            'recordsTotal'    => $this->db->count_all_results('categories'),
+            'recordsTotal'    => $this->db->count_all_results('publishers'),
             'recordsFiltered' => $this->publisher_model->count_all($filter)
         ];
 
         echo json_encode($dataTable, JSON_HEX_AMP | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT);
 	}
 
-	public function index(){
-		$post = $this->input->post();
+	/**
+     * Storing submitted Data to database
+     *
+     * @return void
+     */
+    public function store(): void
+    {
+        $publisher_name   = $this->input->post('publisher_name');
+        $address   = $this->input->post('address');
 
-		if (isset($post['save'])) {
-			$data = [
-				'publisher_name' => $post['publisher_name'],
-				'address' => $post['address'],
-				'created_at' => date('Y-m-d H:i:s'),
-			];
-			$res = $this->Publisher_model->add($data);
-			if ($res) {
-				$this->session->set_flashdata('success', 'Data berhasil ditambahkan');
-			} else {
-				$this->session->set_flashdata('error', 'Data gagal ditambahkan');
-			}
-		}
+        $this->form_validation->set_rules('publisher_name', 'Nama Penerbit', 'required');
 
-		if (isset($post['update'])){
-			$data = [
-				'publisher_name' => $post['publisher_name'],
-				'address' => $post['address'],
-				'updated_at' => date('Y-m-d H:i:s'),
-			];
-			$res = $this->Publisher_model->update($post['id'], $data);
-			if ($res) {
-				$this->session->set_flashdata('success', 'Data berhasil diubah');
-			} else {
-				$this->session->set_flashdata('error', 'Data gagal diubah');
-			}
-		}
+        if(!$this->form_validation->run())
+        {
+            $return = ['success' => false, 'errors' => $this->form_validation->error_array(), 'old' => $_POST];
+            $this->session->set_flashdata('error', $return);
+            redirect($_SERVER['HTTP_REFERER']);
+        }
 
-		if (isset($post['delete'])){
-			$data = [
-				'deleted_at' => date('Y-m-d H:i:s'),
-			];
-			$res = $this->Publisher_model->update($post['id'], $data);
-			if ($res) {
-				$this->session->set_flashdata('success', 'Data berhasil dihapus');
-			} else {
-				$this->session->set_flashdata('error', 'Data gagal dihapus');
-			}
-		}
+        $data = [
+            'publisher_name' => $publisher_name,
+            'address' => $address,
+			'created_at' => date('Y-m-d H:i:s'),
+        ];
 
-		$data['title'] = 'Publisher';
-		$data['publishers'] = $this->publisher_model->get_all();
+        if(!$this->db->insert('publishers', $data))
+        {
+            $return = ['success' => false, 'message' =>  'Data Gagal Di Simpan', 'old' => $_POST];
+            $this->session->set_flashdata('error', $return);
+            redirect($_SERVER['HTTP_REFERER']);
+        }
+       
+       $return = ['success' => true, 'message' =>  'Data Berhasil Di Simpan'];
+       $this->session->set_flashdata('success', $return);
+       redirect($_SERVER['HTTP_REFERER']);
+    }
 
-		echo $this->template->render('index', $data);
-	}
+	/**
+     * Editing data in database
+     *
+     * @return void
+     */
+    public function edit(): void
+    {
+        $id     		= trim($this->input->post('publisher_id', TRUE));
+        $publisher_name	= trim($this->input->post('publisher_name', TRUE));
+        $address 		= trim($this->input->post('address', TRUE));
+
+        $this->form_validation->set_rules('publisher_name', 'Nama Penerbit', 'required');
+
+        if(!$this->form_validation->run())
+        {
+            $return = ['success' => false, 'errors' => $this->form_validation->error_array(), 'old' => $_POST];
+            $this->session->set_flashdata('error', $return);
+            redirect($_SERVER['HTTP_REFERER']);
+        }
+
+        $data = [
+            'publisher_name'   => $publisher_name,
+            'address' => $address,
+            'updated_at'      => date('Y-m-d H:i:s')  
+        ];
+
+        if(!$this->db->update('publishers', $data, ['id' => $id]))
+        {
+            $return = ['success' => false, 'message' =>  'Data Gagal Di Simpan', 'old' => $_POST];
+            $this->session->set_flashdata('error', $return);
+            redirect($_SERVER['HTTP_REFERER']);
+        }
+       
+       $return = ['success' => true, 'message' =>  'Data Berhasil Di Simpan'];
+       $this->session->set_flashdata('success', $return);
+       redirect($_SERVER['HTTP_REFERER']);
+    }
+
+	    /**
+     * Delete data in db
+     *
+     * @return void
+     */
+    public function erase(int $id): void {
+        if(!$this->db->update('publishers', ['deleted_at' => date('Y-m-d H:i:s')], ['id' => $id]))
+        {
+            $return = ['success' => false, 'message' =>  'Data Gagal Di Hapus'];
+            $this->session->set_flashdata('error', $return);
+            redirect($_SERVER['HTTP_REFERER']);
+        }
+
+        $return = ['success' => true, 'message' =>  'Data Berhasil Di Hapus'];
+        $this->session->set_flashdata('success', $return);
+        redirect($_SERVER['HTTP_REFERER']);
+    }
+	
 }
