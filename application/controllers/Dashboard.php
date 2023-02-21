9fef44066e45d6cc127a021b5f02b1301f03003a
fauziahmad37 <fauzi.enginer@gmail.com>
2023-02-21 13:30:09 +0700

    create total member dan total buku pada index Dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,19 +4,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends MY_Controller {
 	public function __construct(){
 		parent::__construct();
-		$this->load->model('User_model');
+		$this->load->model('user_model');
+		$this->load->model('member_model');
+		$this->load->model('book_model');
 		$this->load->library('form_validation');
 		
 	}
 
 	public function index(){
-		$data['user'] = $this->User_model->get_user($this->session->userdata('user')['id']);
-
-		// $this->load->view('header');
-		// $this->load->view('dashboard/index', $data);
-		// $this->load->view('footer');
+		$data['user'] = $this->user_model->get_user($this->session->userdata('user')['id']);
+		$data['total_member'] = count($this->user_model->get_all());
+		$data['total_buku']	= count($this->book_model->get_all());
 
 		// MENGGUNAKAN TEMPLATE ENGINE PLATES
-		echo $this->template->render('index', ['user' => $data['user']]);
+		echo $this->template->render('index', $data);
 	}
 }
