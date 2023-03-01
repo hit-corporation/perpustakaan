58e0e9d0512e0bb3fa4df51dcc73343016afa92e
naquib-hit <naquib@hitcorporation.com>
2023-03-01 17:27:31 +0700

    Add return_date

--- application/views/setting/return_date.php
+++ application/views/setting/return_date.php
@@ -10,9 +10,18 @@
             </div>
             <form class="card-body">
                 <div class="form-row">
-                    <label class="form-label col-6">Tanggal Jatuh Tempo</label>
-                    <div class="col-6">
-                        <input type="date" class="form-control form-control-sm" />
+                    <div class="col-4">
+                        <label class="form-label mb-0">Nilai</label>
+                        <select name="value" class="form-control">
+                            <option value="1">1</option>
+                        </select>
+                    </div>
+                    <div class="col-1 d-flex flex-nowrap justify-content-center align-items-center"><span class="mt-4">-</span></div>
+                    <div class="col-7">
+                        <label class="form-label mb-0">Unit</label>
+                        <select name="unit" class="form-control">
+                            <option value="week">Minggu</option>
+                        </select>
                     </div>
                 </div>    
             </form>
