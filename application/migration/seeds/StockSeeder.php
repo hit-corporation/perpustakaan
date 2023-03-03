9a19c60d916b9d8a0d703774f2e43f6b95400514
fauziahmad37 <fauzi.enginer@gmail.com>
2023-03-03 10:46:38 +0700

    create stock seeder

---
+++ application/migration/seeds/StockSeeder.php
@@ -0,0 +1,41 @@
+<?php
+use Phinx\Seed\AbstractSeed;
+use Faker\Factory as Faker;
+
+class StockSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run(): void
+    {  
+		$data = [
+			[
+				'stock_code' 		=> 'STK-001',
+				'book_id'    		=> 1,
+				'available_status' 	=> 1,
+				'rack_no' 			=> 'R-001'
+			],
+			[
+				'stock_code'		=> 'STK-002',
+				'book_id'    		=> 1,
+				'available_status' 	=> 1,
+				'rack_no' 			=> 'R-002'
+			],
+			[
+				'stock_code' 		=> 'STK-003',
+				'book_id'    		=> 1,
+				'available_status' 	=> 1,
+				'rack_no' 			=> 'R-003'
+			]
+		];
+
+        $table = $this->table('stocks');
+        $table->insert($data)->saveData();
+    }
+}
