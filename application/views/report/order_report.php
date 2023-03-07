77eb6f17996be030c41e714150c27f8d4717e67b
fauziahmad37 <fauzi.enginer@gmail.com>
2023-03-07 10:03:23 +0700

    tambah date range pada fitur laporan peminjaman buku

--- application/views/report/order_report.php
+++ application/views/report/order_report.php
@@ -24,12 +24,32 @@
         <div class="card">
 			<div class="card-header py-3">
 				<div class="row">
-					<div class="col-xl-6 col-lg-6 col-md-6">
+					<div class="col-xl-3 col-lg-3 col-md-3">
 						<h6 class="m-0 font-weight-bold text-primary">List Data Peminjaman Buku</h6>
 
 					</div>
-					<div class="col-xl-6 col-lg-6 col-md-6">
+					<div class="col-xl-4 col-lg-4 col-md-4 mb-2">
 						<form name="form-search">
+							<div class="row">
+								<div class="col-10">
+									<input class="rounded-lg w-100"  style="border-color: rgba(0, 0, 255, 0.3); height: calc(1.5em + 0.5rem + 2px);" type="text" name="daterange" 
+										value="<?php 
+										if(isset($start)){ 
+											echo date('m/d/Y', strtotime($start));
+										} else { 
+											echo date('m', time()).'//1//'.date('Y', time()); 
+										}?> - <?php 
+										if(isset($end)){ 
+											echo date('m/d/Y', strtotime($end)); 
+										}else{ 
+											echo date('m/d/Y', time()); 
+										}?>" />
+								</div>
+							</div>
+						</form>
+					</div>
+					<div class="col-xl-4 col-lg-4 col-md-4">
+						<form name="form-search-name">
 							<div class="row">
 								<div class="col-10">
 									<input type="text" class="form-control form-control-sm" name="s_member_name" placeholder="Nama Member">
@@ -82,5 +102,28 @@
 <?php $this->start('js') ?>
 
 <script src="<?=$this->e(base_url('assets/js/pages/orderReport.js'))?>"></script>
+<script src="<?=base_url('assets/js/jquery.redirect.js')?>"></script>
+
+
+
+<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script> -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+
+
+
+<script>
+// $(function() {
+// 	$('input[name="daterange"]').daterangepicker({
+// 		opens: 'left'
+// 	}, function(start, end, label) {
+// 		$.redirect('<? // =base_url('report')?>', {
+// 			'daterange': start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD'),
+// 		}, 'POST');
+// 	});
+// });
+</script>
 
 <?php $this->stop() ?>
