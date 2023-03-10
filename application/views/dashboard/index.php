aafaa4e26b44c0fafaf1c45c2972cb1ae0108490
fauziahmad37 <fauzi.enginer@gmail.com>
2023-03-10 09:33:33 +0700

    create line chart daily member borrow

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -171,20 +171,14 @@
 
 				<figure class="highcharts-figure" style="height: 417px;">
 					<div id="container"></div>
-					<!-- <p class="highcharts-description">
-						Pie charts are very popular for showing a compact overview of a
-						composition or comparison. While they can be harder to read than
-						column charts, they remain a popular choice for small datasets.
-					</p> -->
 				</figure>
 			</div>
 		</div>
 
 		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
 			<div class="container border rounded-lg shadow p-3">
-				
 				<div id="chartdiv2" style="width: 100%; height: 400px;"></div>
-				</div>
+			</div>
 		</div>
 	</div>
 
@@ -198,7 +192,12 @@
 			</div>
 		</div>
 
-		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 mb-2">
+		<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 mb-2 mt-3">
+			<div class="container border rounded-lg shadow mx-0">
+				<figure class="highcharts-figure">
+					<div id="monyhly-member-borrow"></div>
+				</figure>
+			</div>
 		</div>
 	</div>
 
@@ -272,10 +271,37 @@
 
 <script>
 
-	
-
-	
-
+	// Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
+	Highcharts.chart('monyhly-member-borrow', {
+		chart: {
+			type: 'line'
+		},
+		title: {
+			text: 'Kunjungan Siswa Perbulan'
+		},
+		xAxis: {
+			categories: ['1 feb', '2 feb', '3 feb', '4 feb', '5 feb', '6 feb', '7 feb', '8 feb', '9 feb', '10 feb', '11 feb', '12 feb', '13 feb', '14 feb', '15 feb', '16 feb', '17 feb', '18 feb', '19 feb', '20 feb', '21 feb', '22 feb', '23 feb', '24 feb', '25 feb', '26 feb', '27 feb', '28 feb', '1 mar', '2 mar']
+		},
+		yAxis: {
+			title: {
+				text: 'Total Kunjungan / Hari'
+			}
+		},
+		plotOptions: {
+			line: {
+			dataLabels: {
+				enabled: true
+			},
+			enableMouseTracking: false
+			}
+		},
+		series: [{
+			name: 'Siswa',
+			data: [16, 18, 23, 27, 32, 36, 39, 38, 35, 29,
+			22, 17, 16, 18, 23, 27, 32, 36, 39, 38, 35, 29, 22, 17, 16, 18, 23, 27, 32, 36]
+		},
+	]
+	});
 
 </script>
 
