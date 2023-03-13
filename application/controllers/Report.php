b7e76b65ffd97f9e19e284826b73d26f28294fe2
fauziahmad37 <fauzi.enginer@gmail.com>
2023-03-13 14:19:04 +0700

    create function book, get_all_book and get_all_book_paginated

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -8,6 +8,7 @@ class Report extends MY_Controller
 	{
 		parent::__construct();
 		$this->load->model('transaction_model');
+		$this->load->model('book_model');
 		$this->load->library('form_validation');
 	}
 
@@ -66,6 +67,50 @@ class Report extends MY_Controller
         $data = $this->transaction_model->get_all();
         echo json_encode($data, JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_TAG);
     }
+
+	 /**
+     * function for view book report
+     *
+     * @return void
+     */
+	public function book(): void
+	{
+		echo $this->template->render('report/book_report');
+	}
+
+	/**
+	 * get all books data
+	 *
+	 * @return void
+	 */
+	public function get_all_book(): void
+	{
+		$data = $this->book_model->get_all_book();
+		echo json_encode($data, JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_TAG);
+	}
+
+	/**
+	 * get all paginated data and send as json for datatable consume
+	 *
+	 * @return void
+	 */
+	public function get_all_book_paginated(): void{
+		$limit  = $this->input->get('length');
+		$offset = $this->input->get('start');
+		$filter = $this->input->get('columns');
+
+		// generate data
+		$data = $this->book_model->get_all_book($filter, $limit, $offset);
+
+		$dataTable = [
+			'draw'            => $this->input->get('draw') ?? NULL,
+			'data'            => $data,
+			'recordsTotal'    => $this->db->count_all_results('books'),
+			'recordsFiltered' => $this->book_model->count_all_book($filter)
+		];
+
+		echo json_encode($dataTable, JSON_HEX_AMP | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT);
+	}
 
 }
 
